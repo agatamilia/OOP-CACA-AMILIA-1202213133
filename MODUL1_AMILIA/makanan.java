0f1b1fab794c789864140ba2356fd6ec8f1ec4bd
agatamilia <91583586+agatamilia@users.noreply.github.com>
2022-12-04 07:12:54 +0700

    Add files via upload

---
+++ MODUL1_AMILIA/makanan.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public static void main(String args[]){ 
+    System.out.println("Selamat Datang di Restorant EAD\n");
+    System.out.println("Silahkan Registrasi Terlebih Dahulu\n");
+    System.out.println("-----------------------------------\n");
+}
+
+public class makanan {
+    Int database;
+    String menu;
+    String server;
+    String user;
+
+public void setMenu(String menu){
+    this.menu=menu;
+    
+}
+public void setServer(String server){
+    this.server=server;
+}
+public void setUser(String user){
+    this.user=user;
+    int entry = multi.nextInt();
+
+}
+public void setDatabase(Int database){
+    class menu insert_Menu(){
+    if 
+    this.database=database;
+
+}
+
+}
+}
