b308a30b66ef80c459bbe59116c460d1dc21cfe0
agatamilia <91583586+agatamilia@users.noreply.github.com>
2022-12-26 08:52:13 +0700

    Add files via upload

---
+++ MODUL3_AMILIA/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        Calculation calculation = new Calculation();
+        double radius, side, area, phi;
+        int option = data.nextInt();
+        do{
+            System.out.println("====Menu program==== \n1. square \n2. Circle \n3. Trapezoid");
+            System.out.print("Masukan pilihan: "); 
+            option = data.nextInt();
+            try{
+                switch(option){
+                    case(1): 
+                    System.out.println("");
+                        
+                    
+
+                }
+            }
+
+        }
+                          
+                
+}
